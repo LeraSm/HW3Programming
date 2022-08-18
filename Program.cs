b9a67d4735c6af03898cd354bdee1b3c491a7e39
Lera <badasyan.lera@mail.ru>
2022-08-18 17:45:53 +0300

    added decision of task 23

--- Program.cs
+++ Program.cs
@@ -23,4 +23,13 @@
 //     Console.Write($"{number} is palindrom");
 // }
 
+// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+// Console.Write("input number 1-N: ");
+// int number= Convert.ToInt32(Console.ReadLine());
+// int i= 1;
+// while (i<=number)
+// {
+//     Console.Write(Math.Pow(i,3)+ "|");
+//     i++;
+// }
