cd589bb13fbb0d7d51fa0b1fe1059add094e5943
Lera <badasyan.lera@mail.ru>
2022-08-18 13:09:58 +0300

    added decision for task 19

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Hello, World!");
+
+// Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндр
+
+// Console.Write("input five digit number: ");
+// int number= Convert.ToInt32(Console.ReadLine());
+// if(number>99999 || number<10000)
+// {
+//     Console.Write($"{number} is not five digit");
+//     return;
+// }
+// if(number/10000 != number%10)
+// {
+//     Console.Write($"{number} is not palindrom");
+// }
+// else if((number/1000)%10 != (number%100)/10)
+// {
+//    Console.Write($"{number} is not palindrom");
+
+// }
+// else
+// {
+//     Console.Write($"{number} is palindrom");
+// }
+
+
